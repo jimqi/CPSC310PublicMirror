6c54d49c0c48529aacdff7236d121c77af419830
romastas <anastasiya.ger@gmail.com>
2014-03-05 19:38:42 -0800

    edited version of UserInfo

--- StarvingCodeMonkey/src/entity/UserInfo.java
+++ StarvingCodeMonkey/src/entity/UserInfo.java
@@ -14,6 +14,45 @@ public class UserInfo {
 	private int phoneNum;     // facebook login number
 	private String[] favPlaces; // places rated as favs
 	private String[] placesVisited; // place the user has visited before, with ratings if available
+	public UserInfo(String userName, char[] emailAddr, int phoneNum,
+			String[] favPlaces, String[] placesVisited) {
+		super();
+		this.userName = userName;
+		this.emailAddr = emailAddr;
+		this.phoneNum = phoneNum;
+		this.favPlaces = favPlaces;
+		this.placesVisited = placesVisited;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public char[] getEmailAddr() {
+		return emailAddr;
+	}
+	public void setEmailAddr(char[] emailAddr) {
+		this.emailAddr = emailAddr;
+	}
+	public int getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(int phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String[] getFavPlaces() {
+		return favPlaces;
+	}
+	public void setFavPlaces(String[] favPlaces) {
+		this.favPlaces = favPlaces;
+	}
+	public String[] getPlacesVisited() {
+		return placesVisited;
+	}
+	public void setPlacesVisited(String[] placesVisited) {
+		this.placesVisited = placesVisited;
+	}
 	
 	
 }
