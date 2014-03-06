684b535e148f2f3cd3464910e289e7e4eb194a05
romastas <anastasiya.ger@gmail.com>
2014-03-05 19:24:33 -0800

    changes in UseInfo class

--- StarvingCodeMonkey/src/entity/UserInfo.java
+++ StarvingCodeMonkey/src/entity/UserInfo.java
@@ -2,18 +2,69 @@
  * Store Userinfo to Database 
  */
 package entity;
-//TODO: Kevin
+
+//TODO: Anastasiya
 
 /**
  * @author Charles
- *
+ * 
  */
 public class UserInfo {
-	private String userName;  // first/last name
+	private String userName; // first/last name
 	private char[] emailAddr; // facebook login name
-	private int phoneNum;     // facebook login number
+	private int phoneNum; // facebook login number
 	private String[] favPlaces; // places rated as favs
-	private String[] placesVisited; // place the user has visited before, with ratings if available
-	
-	
+	private String[] placesVisited; // place the user has visited before, with
+									// ratings if available
+
+	public UserInfo(String userName, char[] emailAddr, int phoneNum,
+			String[] favPlaces, String[] placesVisited) {
+		super();
+		this.userName = userName;
+		this.emailAddr = emailAddr;
+		this.phoneNum = phoneNum;
+		this.favPlaces = favPlaces;
+		this.placesVisited = placesVisited;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public char[] getEmailAddr() {
+		return emailAddr;
+	}
+
+	public void setEmailAddr(char[] emailAddr) {
+		this.emailAddr = emailAddr;
+	}
+
+	public int getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(int phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public String[] getFavPlaces() {
+		return favPlaces;
+	}
+
+	public void setFavPlaces(String[] favPlaces) {
+		this.favPlaces = favPlaces;
+	}
+
+	public String[] getPlacesVisited() {
+		return placesVisited;
+	}
+
+	public void setPlacesVisited(String[] placesVisited) {
+		this.placesVisited = placesVisited;
+	}
+
 }
